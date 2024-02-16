b7ef18ab22b7935c1dc0afdb930b40b8be336e56
Sel <selcuk.80@outlook.de>
2024-02-16 12:39:24 +0100

    gg
    gg

---
+++ script.js
@@ -0,0 +1,7 @@
+function addTask(){
+    let task = document.getElementById('task').value;
+    let category = document.getElementById('category').value;
+
+    console.log('Task:',task);
+    console.log('Category:',category);
+}
